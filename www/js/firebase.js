b4ecfa7b36b49a4d2fddb61a6080cc0722bf5af9
MarcosSouza671 <marcos.santos671@etec.sp.gov.br>
2021-10-28 11:56:44 -0300

    versão corrigida

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyBh8UvnDbj9uoph98xBQgLtp_ejviVumf0",
+    authDomain: "testeaula-417e3.firebaseapp.com",
+    projectId: "testeaula-417e3",
+    storageBucket: "testeaula-417e3.appspot.com",
+    messagingSenderId: "742476696416",
+    appId: "1:742476696416:web:240b4b89a11fe8e9ebec05",
+    measurementId: "G-6X3FPDP0K9"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
